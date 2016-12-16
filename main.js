42d5955a217921f1606b80dc15f3a3d2110d4c9e
josh <joshbothun@gmail.com>
2016-12-16 00:03:21 -0800

    Add price buckets and a ticket image

--- main.js
+++ main.js
@@ -1,14 +1,65 @@
+var PRICE_BUCKETS = [
+  {
+    income: 20000,
+    percent: 0.01,
+  },
+  {
+    income: 35000,
+    percent: 0.02,
+  },
+  {
+    income: 60000,
+    percent: 0.03,
+  },
+  {
+    income: 100000,
+    percent: 0.05,
+  },
+  {
+    income: 180000,
+    percent: 0.07,
+  },
+  {
+    income: 270000,
+    percent: 0.1,
+  },
+  {
+    income: 500000,
+    percent: 0.5,
+  },
+  {
+    income: 1000000,
+    percent: 1,
+  },
+];
+
+
 $(function() {
   var income = $('.income');
   var price = $('.price');
+  var ticketPrice = $('.ticket-price');
 
-  var setPrice = function(amount) {
-    price.text('$' + amount);
+  var getPriceFromIncome = function(income) {
+    var lastPercent = 0;
+    for (var i = 0; i < PRICE_BUCKETS.length; i++) {
+      var bucket = PRICE_BUCKETS[i];
+      if (income < bucket.income) {
+        break;
+      }
+      lastPercent = bucket.percent;
+    }
+    return Math.round(income * lastPercent) / 100.0;
   };
 
-  income.keypress(function(event) {
+  var displayPrice = function(amount) {
+    price.text('$' + amount);
+    ticketPrice.text('$' + amount);
+  };
+
+  income.keyup(function(event) {
     // TODO: Add error handling to non-numerical input.
-    var amount = parseInt(event.target.value);
-    setPrice(amount / 100);
+    var income = parseInt(event.target.value);
+    var price = getPriceFromIncome(income);
+    displayPrice(price);
   });
 });
